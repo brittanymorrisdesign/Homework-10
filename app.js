5a6b6f2a09742c3054c39d95d3dc9a3dc025e0bd
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-02-02 22:17:08 -0500

    uploaded npm packages, added manager, engineer, employee, and intern js files

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-$(document).ready(function() {});
+// npm packages
+const inquirer = require('inquirer');
+const util = require('util');
+const fs = require('fs');
+const pdf = require('html-pdf');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const employee = require('./lib/employee');
