beaea43e65593b74829070e64cfda6f6a26ffd17
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-02-04 21:33:28 -0500

    Updated mainhtml to javascript, added prompts

--- app.js
+++ app.js
@@ -10,15 +10,19 @@ const engineerCard = require('./lib/employee');
 const internCard = require('./lib/employee');
 const managerCard = require('./lib/employee');
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // Puts output into the 'output' folder
 const outputPath = path.resolve(__dirname, 'output', 'team.html');
 
+const fullTeam = [];
+
 // Create a manager
- managerPrompt = () => {
+managerPrompt = () => {
   console.log('Who would you like on your team?');
   return inquirer
-    .prompt([
-      {
+    .prompt([{
         type: 'input',
         name: 'managerName',
         message: 'What is your Managers Name?',
@@ -39,78 +43,127 @@ const outputPath = path.resolve(__dirname, 'output', 'team.html');
         message: 'What is your managers office number?',
       },
     ])
-    .then(answers => {
-      const { managerName, managerId, managerEmail, managerNumber } = answers;
-      const managerObject = new Manager(
-        managerName,
-        managerId,
-        managerEmail,
-        managerNumber
-      );
+    .then((data) => {
 
+      const managerObject = new Manager(data.managerName, data.managerId, data.managerEmail, data.officeNumber);
+      console.log(managerObject)
       const managerCardHtml = managerCard(managerObject);
-
-      teamMember.push(managerCardHtml);
-      createTeam();
+      fullTeam.push(managerCardHtml);
+      chooseTeam();
     });
 };
 
-promptUserEngineer = () => {
-    console.log("Create a new engineer profile.")
-    return inquirer
-      .prompt([
-        {
-            type: "input",
-            message: "Enter the name:",
-            name: "name",
-        }, 
-        {
-            type: "input",
-            message: "Enter the id:",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Enter the email:",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Enter the GitHub username:",
-            name: "github",
-}
 
-promptUserIntern = () => {
-    console.log("Create a new intern profile.")
-    return inquirer
-      .prompt([
-        {
-            type: "input",
-            message: "Enter the name:",
-            name: "name",
-        }, 
-        {
-            type: "input",
-            message: "Enter the id:",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Enter the email:",
-            name: "email",
-        },
-        {
-            type: "input",
-            message: "Enter the school:",
-            name: "school",
-        }
-      ]);
-}
+const chooseTeam = () => {
+    inquirer.prompt([{
+        type: "list",
+        name: "member",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I dont want to add any more team members"]
+      }])
+      .then((data) => {
+          if (data.member === "Engineer") {
+            promptUserEngineer();
+
+          } else if (data.member === "Intern") {
+
+            promptUserIntern();
+          } else {
+            console.log("Look in your output folder to see your team!")
+            const outputPage = html.generateHTML(team_cards);
+            fs.writeFile("./output/generateteam.html", outputPage, (err) => {
+                if (err) throw err;
+
+              }
+            }
+
+          }
 
 
+          promptUserEngineer = () => {
+              console.log("Create a new engineer profile.")
+              return inquirer
+                .prompt([{
+                      type: "input",
+                      message: "Enter the name:",
+                      name: "engineerName",
+                    },
+                    {
+                      type: "input",
+                      message: "Enter the id:",
+                      name: "enginnerId",
+                    },
+                    {
+                      type: "input",
+                      message: "Enter the email:",
+                      name: "engineerEmail",
+                    },
+                    {
+                      type: "input",
+                      message: "Enter the GitHub username:",
+                      name: "engineerGithub",
+                    }.then((data) => {
+                    let newEngineer = new Engineer(data.engineerName, data.engineerId, data.engineerEmail, data.engineerGithub);
+                    // console.log(newIntern);
+        
+                    engineerCard = genCard.genInternCard(newIntern.name, newIntern.id, newIntern.email, newIntern.school);
+                    
 
-fs.writeFileSync(outputPath), 'utf-8');
+                    promptUserIntern = () => {
+                      console.log("Create a new intern profile.")
+                      return inquirer
+                        .prompt([{
+                            type: "input",
+                            message: "Enter the name:",
+                            name: "internName",
+                          },
+                          {
+                            type: "input",
+                            message: "Enter the id:",
+                            name: "internId",
+                          },
+                          {
+                            type: "input",
+                            message: "Enter the email:",
+                            name: "internEmail",
+                          },
+                          {
+                            type: "input",
+                            message: "Enter the school:",
+                            name: "internSchool",
+                          }.then((data) => {
+                          let newIntern = new Intern(data.internName, data.internId, data.internEmail, data.internSchool);
+                          // console.log(newIntern);
+              
+                          engineerCard = genCard.genInternCard(newIntern.name, newIntern.id, newIntern.email, newIntern.school);
+                    }
+                  ]);  
 
-managerPrompt();
-promptUserIntern();
-promptUserEngineer();
+
+                    async function combineAnimals() {
+                      try {
+                        const hamster = await readFileAsync("hamster.json", "utf8");
+                        const dog = await readFileAsync("dog.json", "utf8");
+                        const cat = await readFileAsync("cat.json", "utf8");
+                        const goldfish = await readFileAsync("goldfish.json", "utf8");
+
+                        const animalJSON = [hamster, dog, cat, goldfish].map(JSON.parse);
+
+                        await writeFileAsync(
+                          "combined.json",
+                          JSON.stringify(animalJSON, null, 2),
+                          "utf8"
+                        );
+
+                        console.log("Successfully wrote to 'combined.json' file");
+                      } catch (err) {
+                        console.log(err);
+                      }
+                    }
+
+                    combineAnimals();
+
+
+                    managerPrompt();
+                    promptUserIntern();
+                    promptUserEngineer();
