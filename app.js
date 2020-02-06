deca5aea8584ae610f27a9a7fb06ede2e0760385
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-02-05 23:44:50 -0500

    cleaned up code. Added functions

--- app.js
+++ app.js
@@ -16,133 +16,176 @@ const internCard = require('./templates/internhtml');
 const fullTeam = [];
 
 // Create a manager
-const managerPrompt = () => {
+function managerPrompt() {
   console.log('Add a manager');
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'managerName',
-      message: 'What is your Managers Name?',
-    },
-    {
-      type: 'input',
-      name: 'managerId',
-      message: 'What is the employee id?',
-    },
-    {
-      type: 'input',
-      name: 'managerEmail',
-      message: 'What is your managers email?',
-    },
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: 'What is your managers office number?',
-    },
-  ]);
-};
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is your Managers Name?',
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: 'What is the employee id?',
+      },
+      {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is your managers email?',
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is your managers office number?',
+      },
+    ])
+    .then(answers => {
+      const { managerName, managerId, managerEmail, officeNumber } = answers;
+      const managerObj = new Manager(
+        managerName,
+        managerId,
+        managerEmail,
+        officeNumber
+      );
+
+      const managerCardHtml = managerCard(managerObj);
+
+      fullTeam.push(managerCardHtml);
+      promptMain();
+    });
+}
+
+// Main App
+function promptMain() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'userChoice',
+        message: 'What would you like to do?',
+        choices: [
+          'Add an Engineer',
+          'Add an Intern',
+          "I'm all done. Let's see my team!",
+        ],
+      },
+    ])
+    .then(answers => {
+      // console.log(answers.userChoice);
+      // create a switch statement to choose between engineer, intern, or build team
+      switch (answers.userChoice) {
+        case 'Add an Engineer': {
+          engineerPrompt();
+          break;
+        }
+        case 'Add an Intern': {
+          internPrompt();
+          break;
+        }
+        case "I'm all done. Let's see my team!": {
+          promptMain();
+          break;
+        }
+      }
+    });
+}
 
 // Create an engineer
-const engineerPrompt = () => {
+function engineerPrompt() {
   console.log('Add a new Engineer');
-  return inquirer.prompt([
-    {
-      type: 'input',
-      message: 'Enter the name:',
-      name: 'engineerName',
-    },
-    {
-      type: 'input',
-      message: 'Enter the id:',
-      name: 'engineerId',
-    },
-    {
-      type: 'input',
-      message: 'Enter the email:',
-      name: 'engineerEmail',
-    },
-    {
-      type: 'input',
-      message: 'Enter GitHub username:',
-      name: 'engineerGithub',
-    },
-  ]);
-};
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'Enter the name:',
+        name: 'engineerName',
+      },
+      {
+        type: 'input',
+        message: 'Enter the id:',
+        name: 'engineerId',
+      },
+      {
+        type: 'input',
+        message: 'Enter the email:',
+        name: 'engineerEmail',
+      },
+      {
+        type: 'input',
+        message: 'Enter GitHub username:',
+        name: 'engineerGithub',
+      },
+    ])
+    .then(answers => {
+      const {
+        engineerName,
+        engineerId,
+        engineerEmail,
+        engineerGithub,
+      } = answers;
+      const engineerObj = new Engineer(
+        engineerName,
+        engineerId,
+        engineerEmail,
+        engineerGithub
+      );
+      // console.log(engineerObj);
+      const engineerCardHtml = engineerCard(engineerObj);
+      // console.log(engineerCardHtml);
+      fullTeam.push(engineerCardHtml);
+      promptMain();
+    });
+}
 
 // Create an intern
-const internPrompt = () => {
+function internPrompt() {
   console.log('Add a new intern');
-  return inquirer.prompt([
-    {
-      type: 'input',
-      message: 'Enter the name:',
-      name: 'internName',
-    },
-    {
-      type: 'input',
-      message: 'Enter the id:',
-      name: 'internId',
-    },
-    {
-      type: 'input',
-      message: 'Enter the email:',
-      name: 'internEmail',
-    },
-    {
-      type: 'input',
-      message: 'Enter the school:',
-      name: 'internSchool',
-    },
-  ]);
-};
-
-(async function() {
-  try {
-    // Manager info
-    const managerData = await managerPrompt();
-    const userManagerName = managerData.managerName;
-    const userManagerId = managerData.managerId;
-    const userManagerEmail = managerData.managerEmail;
-    const userOfficeNumber = managerData.officeNumber;
-    const newmanager = new Manager(
-      userManagerName,
-      userManagerId,
-      userManagerEmail,
-      userOfficeNumber
-    );
-  } catch (err) {
-    console.log(err);
-
-    // Engineer Info
-    for (let i = 0; i < fullTeam.length; i++) {
-      const engineerData = await engineerPrompt();
-      const userEngineerName = engineerData.engineerMame;
-      const userEngineerId = engineerData.engineerId;
-      const userEngineerEmail = engineerData.engineerEmail;
-      const userEngineerGithub = engineerData.engineergithub;
-      const newEngineer = new Engineer(
-        userEngineerName,
-        userEngineerId,
-        userEngineerEmail,
-        userEngineerGithub
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'Enter the name:',
+        name: 'internName',
+      },
+      {
+        type: 'input',
+        message: 'Enter the id:',
+        name: 'internId',
+      },
+      {
+        type: 'input',
+        message: 'Enter the email:',
+        name: 'internEmail',
+      },
+      {
+        type: 'input',
+        message: 'Enter the school:',
+        name: 'internSchool',
+      },
+    ])
+    .then(answers => {
+      const { internName, internId, internEmail, internSchool } = answers;
+      const internObj = new Intern(
+        internName,
+        internId,
+        internEmail,
+        internSchool
       );
-      fullTeam.push(newEngineer);
-    }
 
-    // Intern info
-    for (let i = 0; i < fullTeam; i++) {
-      const internData = await internPrompt();
-      const userinternName = internData.internName;
-      const userinternId = internData.internId;
-      const userinternEmail = internData.internEmail;
-      const userinternSchool = internData.internSchool;
-      const newIntern = new Intern(
-        userinternName,
-        userinternId,
-        userinternEmail,
-        userinternSchool
-      );
-      fullTeam.push(newIntern);
-    }
-  }
-})();
+      const internCardHtml = internCard(internObj);
+
+      fullTeam.push(internCardHtml);
+      promptMain();
+    });
+}
+
+function buildTeam() {
+  // remove commas from the array
+  const joinedTeam = fullTeam.join('');
+
+  fs.writeFileSync(outputPath, mainRender(joinedTeam), 'utf-8');
+}
+
+managerPrompt();
