57331d9724945a4dd9314aea776d942991347846
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-02-05 21:57:27 -0500

    No errors. Added async await

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const fs = require('fs');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
-const employee = require('./lib/employee');
-const engineerCard = require('./lib/employee');
-const internCard = require('./lib/employee');
-const managerCard = require('./lib/employee');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -17,93 +13,53 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const fullTeam = [];
 
 // Create a manager
-function managerPrompt() {
+const managerPrompt = () => {
+  console.log('You must create 1 manger profile.');
   return inquirer.prompt([
     {
       type: 'input',
       name: 'managerName',
       message: 'What is your Managers Name?',
+      validate(value) {
+        const valid = isNaN(value);
+        return valid || 'Please enter a name.';
+      },
     },
     {
       type: 'input',
       name: 'managerId',
       message: 'What is the employee id?',
+      validate(value) {
+        const valid = !isNaN(parseFloat(value));
+        return valid || 'Please enter a name.';
+      },
     },
     {
       type: 'input',
       name: 'managerEmail',
       message: 'What is your managers email?',
+      validate(value) {
+        const valid = isNaN(value);
+        return valid || 'Please enter an email.';
+      },
     },
     {
       type: 'input',
       name: 'officeNumber',
       message: 'What is your managers office number?',
+      validate(value) {
+        const valid = !isNaN(parseFloat(value));
+        return valid || 'Please enter a number.';
+      },
     },
   ]);
-}
+};
 
-function mainPrompt() {
-  inquirer.prompt([
-    {
-      type: 'list',
-      name: 'member',
-      message: 'Which type of team member would you like to add?',
-      choices: [
-        'Engineer',
-        'Intern',
-        'I dont want to add any more team members',
-      ],
-    },
-  ]);
-}
-
-function prompEngineer() {
-  inquirer.prompt([
-    {
-      type: 'input',
-      message: 'Enter the name:',
-      name: 'engineerName',
-    },
-    {
-      type: 'input',
-      message: 'Enter the id:',
-      name: 'enginnerId',
-    },
-    {
-      type: 'input',
-      message: 'Enter the email:',
-      name: 'engineerEmail',
-    },
-    {
-      type: 'input',
-      message: 'Enter the GitHub username:',
-      name: 'engineerGithub',
-    },
-  ]);
-}
-
-function prompIntern() {
-  inquirer.prompt([
-    {
-      type: 'input',
-      message: 'Enter the name:',
-      name: 'internName',
-    },
-    {
-      type: 'input',
-      message: 'Enter the id:',
-      name: 'internId',
-    },
-    {
-      type: 'input',
-      message: 'Enter the email:',
-      name: 'internEmail',
-    },
-    {
-      type: 'input',
-      message: 'Enter the school:',
-      name: 'internSchool',
-    },
-  ]);
-  managerPrompt();
-}
+(async function() {
+  try {
+    // Wait on user's data on 1 manager and then store input into manager object
+    const managerData = await managerPrompt();
+  } catch (err) {
+    console.log(err);
+  }
+})();
