f53c01aaba578df351c1fa7ffa9c313487da7ca6
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-02-05 23:33:57 -0500

    added html cards

--- app.js
+++ app.js
@@ -3,63 +3,146 @@ const inquirer = require('inquirer');
 const util = require('util');
 const fs = require('fs');
 
+// Classes
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+// Cards
+const managerCard = require('./templates/managerhtml');
+const engineerCard = require('./templates/engineerhtml');
+const internCard = require('./templates/internhtml');
 
 const fullTeam = [];
 
 // Create a manager
 const managerPrompt = () => {
-  console.log('You must create 1 manger profile.');
+  console.log('Add a manager');
   return inquirer.prompt([
     {
       type: 'input',
       name: 'managerName',
       message: 'What is your Managers Name?',
-      validate(value) {
-        const valid = isNaN(value);
-        return valid || 'Please enter a name.';
-      },
     },
     {
       type: 'input',
       name: 'managerId',
       message: 'What is the employee id?',
-      validate(value) {
-        const valid = !isNaN(parseFloat(value));
-        return valid || 'Please enter a name.';
-      },
     },
     {
       type: 'input',
       name: 'managerEmail',
       message: 'What is your managers email?',
-      validate(value) {
-        const valid = isNaN(value);
-        return valid || 'Please enter an email.';
-      },
     },
     {
       type: 'input',
       name: 'officeNumber',
       message: 'What is your managers office number?',
-      validate(value) {
-        const valid = !isNaN(parseFloat(value));
-        return valid || 'Please enter a number.';
-      },
+    },
+  ]);
+};
+
+// Create an engineer
+const engineerPrompt = () => {
+  console.log('Add a new Engineer');
+  return inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Enter the name:',
+      name: 'engineerName',
+    },
+    {
+      type: 'input',
+      message: 'Enter the id:',
+      name: 'engineerId',
+    },
+    {
+      type: 'input',
+      message: 'Enter the email:',
+      name: 'engineerEmail',
+    },
+    {
+      type: 'input',
+      message: 'Enter GitHub username:',
+      name: 'engineerGithub',
+    },
+  ]);
+};
+
+// Create an intern
+const internPrompt = () => {
+  console.log('Add a new intern');
+  return inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Enter the name:',
+      name: 'internName',
+    },
+    {
+      type: 'input',
+      message: 'Enter the id:',
+      name: 'internId',
+    },
+    {
+      type: 'input',
+      message: 'Enter the email:',
+      name: 'internEmail',
+    },
+    {
+      type: 'input',
+      message: 'Enter the school:',
+      name: 'internSchool',
     },
   ]);
 };
 
 (async function() {
   try {
-    // Wait on user's data on 1 manager and then store input into manager object
+    // Manager info
     const managerData = await managerPrompt();
+    const userManagerName = managerData.managerName;
+    const userManagerId = managerData.managerId;
+    const userManagerEmail = managerData.managerEmail;
+    const userOfficeNumber = managerData.officeNumber;
+    const newmanager = new Manager(
+      userManagerName,
+      userManagerId,
+      userManagerEmail,
+      userOfficeNumber
+    );
   } catch (err) {
     console.log(err);
+
+    // Engineer Info
+    for (let i = 0; i < fullTeam.length; i++) {
+      const engineerData = await engineerPrompt();
+      const userEngineerName = engineerData.engineerMame;
+      const userEngineerId = engineerData.engineerId;
+      const userEngineerEmail = engineerData.engineerEmail;
+      const userEngineerGithub = engineerData.engineergithub;
+      const newEngineer = new Engineer(
+        userEngineerName,
+        userEngineerId,
+        userEngineerEmail,
+        userEngineerGithub
+      );
+      fullTeam.push(newEngineer);
+    }
+
+    // Intern info
+    for (let i = 0; i < fullTeam; i++) {
+      const internData = await internPrompt();
+      const userinternName = internData.internName;
+      const userinternId = internData.internId;
+      const userinternEmail = internData.internEmail;
+      const userinternSchool = internData.internSchool;
+      const newIntern = new Intern(
+        userinternName,
+        userinternId,
+        userinternEmail,
+        userinternSchool
+      );
+      fullTeam.push(newIntern);
+    }
   }
 })();
